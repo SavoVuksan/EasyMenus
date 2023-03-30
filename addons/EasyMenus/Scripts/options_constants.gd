49ba83ee5561fc92ea00859491b3a961d899d4c0
zxtion <LookASupernova@gmail.com>
2023-03-29 21:55:46 -0700

    Generate volume sliders from project buses
    Instead of hardcoding a Music and SFX bus, this will instead generate sliders based on the audio buses already defined in the project.

--- addons/EasyMenus/Scripts/options_constants.gd
+++ addons/EasyMenus/Scripts/options_constants.gd
@@ -1,13 +1,9 @@
 extends Node
 class_name OptionsConstants
 
-const sfx_bus_name = "SFX"
-const music_bus_name = "Music" 
 const config_file_name = "user://options.cfg"
 
 const section_name = "Options" 
-const sfx_volume_key_name = "sfx_volume"
-const music_volume_key_name = "music_volume" 
 const fullscreen_key_name = "fullscreen"
 const render_scale_key = "render_scale"
 const vsync_key = "vsync"
